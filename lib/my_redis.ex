e7d5a6fcb69f578116a45066944642d698c087d3
Jechol Lee <mr.jechol@gmail.com>
2022-06-10 11:32:14 +0900

    completed

--- lib/my_redis.ex
+++ lib/my_redis.ex
@@ -1,17 +1,21 @@
 defmodule MyRedis do
-  use GenServer
+  use Agent
   # 아래와 같은 API 를 가지는 MyRedis 를 GenServer 기반으로 구현하세요.
   # mix test 를 통과하면 구현이 완료된 것입니다.
 
   def start_link() do
+    Agent.start(fn -> %{} end)
   end
 
   def get(pid, key) do
+    Agent.get(pid, fn map -> map |> Map.fetch(key) end)
   end
 
   def set(pid, key, value) do
+    Agent.update(pid, fn map -> map |> Map.put(key, value) end)
   end
 
   def delete(pid, key) do
+    Agent.update(pid, fn map -> map |> Map.delete(key) end)
   end
 end
